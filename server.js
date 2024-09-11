4787fb5cdfb4265f59a7adc75a9abef0a04d1877
ohnggni <33609687+ohnggni@users.noreply.github.com>
2024-09-11 21:51:00 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const path = require('path');
 const app = express();
 const port = 3006; // 새롭게 사용하는 포트
 
-// 정적 파일 제공을 위한 경로 설정 (frontend 폴더 사용)
+// 정적 파일 제공을 위한 경로 설정
 app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use('/epg', express.static(path.join(__dirname, 'epg'))); // EPG 경로 설정
+app.use(express.static(__dirname)); // 기본 경로 설정
 
 // 기본 경로로 index.html 제공
 app.get('/', (req, res) => {
