c1f1fc0b2b48bfa1fa12b377a246c5b2846cef58
ohnggni <33609687+ohnggni@users.noreply.github.com>
2024-09-11 21:47:24 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3006; // 새롭게 사용하는 포트
 
 // 정적 파일 제공을 위한 경로 설정 (frontend 폴더 사용)
-app.use(express.static(path.join(__dirname, '.')));
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
 // 기본 경로로 index.html 제공
 app.get('/', (req, res) => {
