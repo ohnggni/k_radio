234eccd4fbd306403bcebda37b6909ae4fe15a88
ohnggni <33609687+ohnggni@users.noreply.github.com>
2024-09-12 13:45:11 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3006; // 새롭게 사용하는 포트
-const serverIp = process.env.SERVER_IP || 'localhost';
+const serverIp = process.env.SERVER_IP || 'localhost'; // 도커환경 변수에서 server ip 가져옴
 
 // 정적 파일 제공을 위한 경로 설정
 app.use('/static', express.static(path.join(__dirname, 'static')));
@@ -12,6 +12,12 @@ app.use(express.static(__dirname)); // 기본 경로 설정
 // 기본 경로로 index.html 제공
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// 서버 IP를 클라이언트에 전달하기 위해 변수 삽입
+app.get('/config.js', (req, res) => {
+    res.type('.js');
+    res.send(`const SERVER_IP = '${serverIp}';`);
 });
 
 app.listen(port, '0.0.0.0', () => {
